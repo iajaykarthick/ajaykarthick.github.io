e62c414eb5833ed188b88778cf044788c8a2dd86
Ajay Karthick <24907339+iajaykarthick@users.noreply.github.com>
2021-04-27 15:37:31 +0530

    Added header-nav and first main section

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { NavbarComponent } from './header/navbar/navbar.component';
+import { Section1MainComponent } from './header/section1-main/section1.main.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    NavbarComponent,
+    Section1MainComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/header/section1-main/section1.main.component.ts
@@ -0,0 +1,8 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: '<app-section1-main>',
+  templateUrl: './section1.main.component.html',
+  styleUrls: ['./section1.main.component.css']
+})
+export class Section1MainComponent {}
