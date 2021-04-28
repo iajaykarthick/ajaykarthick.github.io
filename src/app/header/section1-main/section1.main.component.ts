80098654ccdd0d8bf7d0841f280fc1e25e9aa230
Ajay Karthick <24907339+iajaykarthick@users.noreply.github.com>
2021-04-28 20:10:21 +0530

    Completed Section 1

--- src/app/header/section1-main/section1.main.component.ts
+++ src/app/header/section1-main/section1.main.component.ts
@@ -1,8 +1,31 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+import Typewriter from 't-writer.js'
 
 @Component({
   selector: '<app-section1-main>',
   templateUrl: './section1.main.component.html',
   styleUrls: ['./section1.main.component.css']
 })
-export class Section1MainComponent {}
+export class Section1MainComponent implements OnInit {
+
+  ngOnInit(): void {
+    const target = document.querySelector('.exp-writer');
+    const expWriter = new Typewriter(target, {
+      loop: true,
+      typeSpeed: 100,
+      deleteSpeed: 100,
+      typeColor: 'white',
+      cursorColor: 'green'
+    });
+
+    expWriter
+    .strings(
+      400,
+      "Angular",
+      "Node.Js",
+      "Python",
+      "Java"
+    )
+    .start()
+  }
+}
---
+++ src/app/header/terminal/terminal.component.ts
@@ -0,0 +1,99 @@
+import { Component, OnInit } from "@angular/core";
+import Typewriter from 't-writer.js'
+
+@Component({
+  selector: '<app-terminal>',
+  templateUrl: './terminal.component.html',
+  styleUrls: ['./terminal.component.css']
+})
+export class TerminalComponent implements OnInit {
+
+  commandsOutput=[
+    { command: "whoami", output: "ajay_karthick"},
+    { command: "pwd", output: "/home/ajay_karthick/resume"},
+    { command: "ls", output: "academics work projects skills"}
+  ];
+  commandWriter: Typewriter;
+  outputWriter: Typewriter;
+  clearWriter: Typewriter;
+  console_sign: "> " | "";
+  index = 0;
+
+  ngOnInit(): void {
+    this.writeCommandOutput();
+  }
+
+  writeCommandOutput() {
+    const target1 = document.querySelector('.command');
+    const target2 = document.querySelector('.cmd-output');
+    const target3 = document.querySelector('.clear-console');
+
+    this.commandWriter = new Typewriter(target1, {
+      typeSpeed: 250,
+      animateCuror: false,
+      loop: false,
+      typeColor: '#d8e3e7',
+      cursorColor: '#d8e3e7'
+    });
+
+    this.outputWriter = new Typewriter(target2, {
+      typeSpeed: 0,
+      loop: false,
+      typeColor: '#a6f0c6',
+      cursorColor: 'black'
+    });
+
+    this.clearWriter = new Typewriter(target3, {
+      typeSpeed: 250,
+      loop: false,
+      typeColor: '#d8e3e7',
+      cursorColor: '#d8e3e7'
+    });
+
+    this.commandWriter
+      .type(this.getNextCommand())
+      .rest(500)
+      .removeCursor()
+      .then(this.outputWriter.start.bind(this.outputWriter))
+      .start()
+
+    this.outputWriter
+      .type(this.getNextOutput())
+      .removeCursor()
+      .rest(100)
+      .then(() => {this.console_sign="> "; this.clearWriter.start()})
+
+    this.clearWriter
+      .rest(2000)
+      .type("clear")
+      .rest(500)
+      .removeCursor()
+      .then(() => {this.clear()})
+  }
+
+  getNextCommand() {
+    return this.commandsOutput[this.index]["command"];
+  }
+
+  getNextOutput() {
+    const output = this.commandsOutput[this.index]["output"];
+    console.log(output, this.index)
+    if (this.index == this.commandsOutput.length - 1) {
+      this.index = 0;
+    } else {
+      this.index += 1;
+    }
+    return output;
+  }
+
+  clear() {
+    this.outputWriter.clearText();
+    this.commandWriter.clearText();
+    this.clearWriter.clearText();
+    this.outputWriter = null;
+    this.commandWriter = null;
+    this.clearWriter = null;
+    this.console_sign = "";
+    this.writeCommandOutput();
+  }
+}
